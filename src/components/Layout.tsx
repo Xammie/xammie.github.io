79ff9f31b45d0aac4ca3f3e92ac627c05f2c121c
Max Hoogenbosch <max.hoogenbosch@gmail.com>
2020-10-28 18:23:18 +0100

    Added meta theme-color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,14 +3,15 @@ import {Helmet} from "react-helmet";
 
 export default function Layout({children}) {
     return (
-            <div className="min-h-screen bg-blue-600 text-white relative">
-                <Helmet>
-                    <html lang="en"/>
-                    <meta charSet="utf-8"/>
-                    <meta name="description" content="Fullstack developer from the Netherlands"/>
-                    <title>Max</title>
-                </Helmet>
-                {children}
-            </div>
+        <div className="min-h-screen bg-blue-600 text-white relative">
+            <Helmet>
+                <html lang="en"/>
+                <meta charSet="utf-8"/>
+                <meta name="description" content="Fullstack developer from the Netherlands"/>
+                <meta name="theme-color" content="#1a202c"/>
+                <title>Max</title>
+            </Helmet>
+            {children}
+        </div>
     );
 }
