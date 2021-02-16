2c99d18811ff981fdadd22c3fd8ec4c436031d34
Max Hoogenbosch <mhoogenbosch@enrise.com>
2021-02-16 22:45:55 +0100

    Added story for Socials component

---
+++ src/stories/Socials.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Socials from '../components/Socials';
+
+export default {
+    title: 'Components/Socials',
+    decorators: [(Story) => <div className="flex"><Story/></div>],
+    parameters: {
+        layout: 'centered',
+    },
+};
+
+export {Socials}
