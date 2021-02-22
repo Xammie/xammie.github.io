6716f6f11cd99a7b66d8d65bcd6cf303f3613ffb
Max Hoogenbosch <mhoogenbosch@enrise.com>
2021-02-22 19:51:16 +0100

    Removed dark mode transition

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function Layout({children}) {
     return (
-        <div className="min-h-screen bg-gray-100 dark:bg-background relative transition-colors duration-300"
+        <div className="min-h-screen bg-gray-100 dark:bg-background relative"
              style={{minHeight: '-webkit-fill-available'}}>
             {children}
             <ThemeToggle/>
