a3b9a1c2ad9a0bfd002f9dfd4f45e5bc9eb5e539
Max Hoogenbosch <mhoogenbosch@enrise.com>
2021-02-23 23:10:15 +0100

    Fixed blog story

--- src/stories/Blog.stories.tsx
+++ src/stories/Blog.stories.tsx
@@ -12,18 +12,16 @@ export default {
     }
 };
 
-const blogData = {
-    remark: {
-        frontmatter: {
-            title: 'This is a random test blog',
-            slug: '/blog/test',
-            tags: ['random', 'tags'],
-            date: 'January 1, 2021',
-        },
-        excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur venenatis dignissim leo et pulvinar. Praesent dictum congue luctus. In consequat velit ut tempus tristique. Maecenas eget justo tortor.',
-    }
+const data = {
+    frontmatter: {
+        title: 'This is a random test blog',
+        slug: '/blog/test',
+        tags: ['random', 'tags'],
+        date: 'January 1, 2021',
+    },
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur venenatis dignissim leo et pulvinar. Praesent dictum congue luctus. In consequat velit ut tempus tristique. Maecenas eget justo tortor.',
 };
 
-export const BlogList = () => <List data={{nodes: [blogData, blogData, blogData], pageInfo: {hasNextPage: true}}}/>
+export const BlogList = () => <List data={{nodes: [data, data, data], pageInfo: {hasNextPage: true}}}/>
 
-export const BlogItem = () => <Card><Item data={blogData.remark}/></Card>
+export const BlogItem = () => <Card><Item data={data}/></Card>
